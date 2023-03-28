e7fbe6119d52bf5403998e2b5438ed9a19317526
9411z <66051647+9411z@users.noreply.github.com>
2023-03-28 15:56:09 +0800

    Update timer_util.h

--- timer_util.h
+++ timer_util.h
@@ -1,3 +1,143 @@
+#pragma once
+
+#include "ebag_util.h"
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <functional>
+#include <inttypes.h>
+#include <iostream>
+#include <list>
+#include <math.h>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+
+namespace pavaro {
+namespace util {
+class Timer;
+class TimerTask;
+
+using TimerPtr         = std::shared_ptr<Timer>;
+using TimePoint_t      = std::chrono::steady_clock::time_point;
+using Clock_t          = std::chrono::steady_clock;
+using MillisDuration_t = std::chrono::milliseconds;
+using TimerTaskPtr     = std::shared_ptr<TimerTask>;
+
+using TimerTaskQueue =
+    std::priority_queue<TimerTaskPtr, std::deque<TimerTaskPtr>, util::SpGreater<TimerTaskPtr>>;
+
+enum {
+    VIRGIN    = 0,
+    SCHEDULED = 1,
+    EXECUTED  = 2,
+    CANCELLED = 3,
+};
+
+class TimerTask {
+private:
+    int64_t task_id;
+    int32_t state = VIRGIN;
+
+    TimePoint_t nextExecutionTime;
+
+    MillisDuration_t period;
+
+    std::function<void(TimerTaskPtr &t)> run_;
+
+    std::string name;
+
+    static int64_t asignTaskId() {
+        static std::atomic_int64_t id;
+        return id == INT64_MAX ? id = 0 : ++id;
+    }
+
+public:
+    TimerTask(std::string &&n) : task_id(asignTaskId()), state(VIRGIN), name(std::move(n)) {}
+    TimerTask(const std::string &n) : task_id(asignTaskId()), state(VIRGIN), name(n) {}
+    TimerTask() : TimerTask("") {}
+    TimerTask(const TimerTask &t) {
+        nextExecutionTime = t.nextExecutionTime;
+        period            = t.period;
+        run_              = t.run_;
+        name              = t.name;
+    }
+    TimerTask &operator=(const TimerTask &t) {
+        if (&t == this)
+            return *this;
+        run_              = t.run_;
+        nextExecutionTime = t.nextExecutionTime;
+        period            = t.period;
+        name              = t.name;
+        return *this;
+    }
+
+    TimerTask &operator=(TimerTask &&t) {
+        if (&t == this)
+            return *this;
+
+        run_              = std::move(t.run_);
+        nextExecutionTime = std::move(t.nextExecutionTime);
+        period            = std::move(t.period);
+        name              = t.name;
+        return *this;
+    }
+
+    void onRun(std::function<void(TimerTaskPtr &t)> &&callback) { run_ = std::move(callback); }
+    void run(TimerTaskPtr &t) {
+        if (run_ != nullptr)
+            run_(t);
+    }
+
+    ~TimerTask() {
+        state = CANCELLED;
+        MLOG(Bag, INFO) << "TimerTask deconstruct !";
+    }
+    inline bool cancel() {
+        bool result = (state == SCHEDULED);
+        state       = CANCELLED;
+        return result;
+    }
+
+    inline int32_t State() { return state; }
+    inline void setState(int32_t s) { state = s; }
+
+    bool operator<(const TimerTask &t2) { return nextExecutionTime < t2.nextExecutionTime; }
+    bool operator>(const TimerTask &t2) { return nextExecutionTime > t2.nextExecutionTime; }
+
+    bool operator==(const TimerTask &t2) {
+        if (task_id == t2.task_id)
+            return true;
+
+        return false;
+    }
+
+    int64_t getTaskId() const { return task_id; }
+
+    void reload() { nextExecutionTime += period; }
+
+    const TimePoint_t &schedTime() const { return nextExecutionTime; }
+
+    void setName(const std::string n) { name = n; }
+    void setName(const char *n) { name = n; }
+
+    void setSchedDelay(int64_t delay) {
+        nextExecutionTime = Clock_t::now() + MillisDuration_t(delay);
+    }
+
+    void setSchedTime(const TimePoint_t &tp) { nextExecutionTime = tp; }
+
+    const MillisDuration_t &repeatPeriod() const { return period; }
+
+    void setRepeatPeriod(int64_t p) { period = MillisDuration_t(p); }
+
+    void setRepeatPeriod(const MillisDuration_t &p) { period = p; }
+
+    const std::string &taskName() { return name; }
+};
+
 class Timer {
 private:
     TimerTaskQueue queue;
@@ -128,3 +268,5 @@ private:
         notify();
     }
 };
+} // namespace util
+} // namespace pavaro
